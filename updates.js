ad5fb7286cc2e8668b9fe97ab7566f6aff06fccf
silverwind <me@silverwind.io>
2020-10-05 23:33:00 +0200

    fix imports

--- updates.js
+++ updates.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 const ansiRegex = require("ansi-regex");
 const dns = require("dns");
-const fetchEnhanced = require("../fetch-enhanced");
+const fetch = require("fetch-enhanced")(require("node-fetch"));
 const minimist = require("minimist");
-const nodeFetch = require("node-fetch");
 const rat = require("registry-auth-token");
 const rc = require("rc");
 const ru = require("registry-auth-token/registry-url");
@@ -15,8 +14,6 @@ const {join, dirname} = require("path");
 const {lstatSync, readFileSync, truncateSync, writeFileSync, accessSync} = require("fs");
 const {platform} = require("os");
 const {version} = require("./package.json");
-
-const fetch = fetchEnhanced(nodeFetch);
 
 env.NODE_ENV = "production";
 
