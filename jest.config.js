788bdb18aceb72350beb8504ef0888a489f331c0
silverwind <me@silverwind.io>
2021-10-14 14:03:35 +0200

    update deps

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   modulePathIgnorePatterns: ["<rootDir>/bin"],
-  setupFilesAfterEnv: ["jest-extended"],
+  setupFilesAfterEnv: ["jest-extended/all"],
   testTimeout: 60000,
   transform: {},
   verbose: false
