00efbd04a8077fde46e39a3ed849ed31cb4a670c
silverwind <me@silverwind.io>
2023-04-24 18:20:47 +0200

    remove todo

--- updates.js
+++ updates.js
@@ -661,7 +661,6 @@ async function main() {
     finish(new Error(`Error parsing ${packageFile}: ${err.message}`));
   }
 
-  // TODO: merge cli and config options for these
   let include, exclude;
   if (args.include && args.include !== true) {
     include = new Set(((Array.isArray(args.include) ? args.include : [args.include]).flatMap(item => item.split(","))));
