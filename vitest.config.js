e536b0f37832ff39a8688cc476da80915d461ac0
silverwind <me@silverwind.io>
2024-04-13 20:16:14 +0200

    update deps

--- vitest.config.js
+++ vitest.config.js
@@ -1,4 +1,6 @@
 import {defineConfig} from "vitest/dist/config.js";
 import {backend} from "vitest-config-silverwind";
 
-export default defineConfig(backend({url: import.meta.url}));
+export default defineConfig(backend({
+  url: import.meta.url,
+}));
