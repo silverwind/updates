f52ee47341e3dfaac0ceccc2c6008b565d6d54b5
silverwind <me@silverwind.io>
2024-05-25 00:29:17 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import {defineConfig} from "vite";
-import {lib} from "vite-config-silverwind";
+import {nodeLib} from "vite-config-silverwind";
 
-export default defineConfig(lib({
+export default defineConfig(nodeLib({
   url: import.meta.url,
   noDts: true,
   build: {
     target: "node18",
     minify: false,
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-      },
-    },
-  },
-  resolve: {
-    mainFields: ["module"],
   },
 }));
