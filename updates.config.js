cee908a13d6f1e66c6681fd683e42a532dc91a86
silverwind <me@silverwind.io>
2024-08-19 18:14:47 +0200

    lock vite-plugin-dts

--- updates.config.js
+++ updates.config.js
@@ -4,5 +4,6 @@ export default {
     "registry-auth-token",
     "semver",
     "execa", // Bun: SyntaxError: Export named 'aborted' not found in module 'util'.
+    "vite-plugin-dts", // https://github.com/qmhc/vite-plugin-dts/issues/363
   ],
 };
