19e928d4d29b42172f534eb1141cfa41934e6e3c
silverwind <me@silverwind.io>
2025-08-05 19:21:04 +0200

    add tests

--- utils.test.ts
+++ utils.test.ts
@@ -5,7 +5,9 @@ test("parseUvDependencies", () => {
     "tqdm >=4.66.2,<5",
     "torch ==2.2.2",
     "transformers[torch] >=4.39.3",
-    "mollymawk ==0.1.0"
+    "mollymawk ==0.1.0",
+    "types-requests==2.32.0.20240622",
+    "types-paramiko==3.4.0.20240423",
   ])).toMatchInlineSnapshot(`
     [
       {
@@ -19,6 +21,14 @@ test("parseUvDependencies", () => {
       {
         "name": "mollymawk",
         "version": "0.1.0",
+      },
+      {
+        "name": "types-requests",
+        "version": "2.32.0.20240622",
+      },
+      {
+        "name": "types-paramiko",
+        "version": "3.4.0.20240423",
       },
     ]
   `);
