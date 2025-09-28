149a9106a01ee7be5eb84415fbb4517124d93817
silverwind <me@silverwind.io>
2025-09-29 01:30:21 +0200

    remove workaround

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,6 @@ import {defineConfig} from "tsdown";
 
 export default defineConfig(nodeLib({
   url: import.meta.url,
-  entry: "index.ts", // https://github.com/rolldown/tsdown/issues/518
   minify: true,
   sourcemap: false,
 }));
