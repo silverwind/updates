c93af2c73cdb894f660f6c253a0f054f384bd113
silverwind <me@silverwind.io>
2026-01-29 10:49:50 +0100

    Apply suggestion from @silverwind

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,6 @@
 import silverwind from "eslint-config-silverwind";
 import {defineConfig} from "eslint/config";
 
-export default defineConfig(
-  ...silverwind,
-  {
-    ignores: ["fixtures/invalid-config/**"],
-  },
-);
+export default defineConfig(...silverwind, {
+  ignores: ["fixtures/invalid-config/**"],
+});
